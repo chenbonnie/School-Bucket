404879004a0a246f34c9b4334309577f05b4e4bd
Anna Bowers <anna.bowers.ab@gmail.com>
2021-01-31 03:16:48 -0600

    Add files via upload
    fixing up main some more. really need to put it in that class I had made and clean that up but we don't really have as much time as I wanted for that.

--- main.cpp
+++ main.cpp
@@ -17,60 +17,77 @@ using std::cout;
 using std::vector;
 using std::to_string;
 
+unsigned int findStringReturnIndex(vector<char> giantCharVector, string myString) {
+	unsigned int stringLength = myString.length();
+	unsigned int vectorLength = giantCharVector.size();
 
-// TODO: clean up hour:minute
+	unsigned int countNumMatchingChars = 0;
+
+	for (unsigned int i = 0; i < vectorLength; i++) {
+
+		for (unsigned int j = 0; j < stringLength; j++) {
+			// if two chars are equal
+			if (giantCharVector.at(i) == myString.at(j)) {
+				// then check the next two chars.
+				i++;
+				countNumMatchingChars++;
+
+				if (countNumMatchingChars == stringLength - 1) {
+					// go back to the beginning
+					return i - stringLength;
+				}
+
+
+			} else {
+				// start over with the first of myString
+				// and start with next of vector
+				i++;
+				j = 0;
+				countNumMatchingChars = 0;
+			}
+		}	
+	}
+
+	return 0;
+}
 
 
 // if on either side of ':', there are integers, then 
 // mark that spot as potientially a time. save the index somewhere.
 vector<unsigned int> potentialTimeAtThisIndex(vector<char> giantCharVector) {
-
-
 	vector<unsigned int> listOfIndexesPotentialTimes;
-
 	for (unsigned int i = 0; i < giantCharVector.size(); i++){
-		if ( (giantCharVector[i] >= 48 && giantCharVector[i] <= 57) &&
-		     (giantCharVector[i + 1] == ':') &&
-		     (giantCharVector[i + 2] >= 48 && giantCharVector[i + 2] <= 57)) {
-			
+		if ( (giantCharVector.at(i) >= 48 && giantCharVector.at(i) <= 57) &&
+		     (giantCharVector.at(i + 1) == ':') &&
+		     (giantCharVector.at(i + 2) >= 48 && giantCharVector.at(i + 2) <= 57)) {
+		
 			listOfIndexesPotentialTimes.push_back(i + 1);
-
 		}
 	}
 
 	return listOfIndexesPotentialTimes;
 
-
-
 }
 
 
 // now this time have index be the first letter of what you think is a day of the week
-vector<unsigned int> potentialDayAtThisIndex(vector<char> giantCharVector) {
-
+vector<unsigned int> potentialDayAtThisIndex(vector<char> giantCharVector, vector<string> weekdays) {
 
 	vector<unsigned int> listOfIndexesPotentialDays;
 
-	// FIXME(this MWF stuff), connect to text files lists
-	for (unsigned int i = 0; i < giantCharVector.size(); i++){
-		if ( (giantCharVector[i] == 'M') &&
-		     (giantCharVector[i + 1] == 'W') &&
-		     (giantCharVector[i + 2] == 'F')) {
-			
-			listOfIndexesPotentialDays.push_back(i);
+	string myString;
 
-		} else if ( (giantCharVector[i] == 'M') &&
-		     (giantCharVector[i + 1] == 'W')){
-			listOfIndexesPotentialDays.push_back(i);
+	for (unsigned int i = 0; i < giantCharVector.size(); i++) {
 
-		} else if ( (giantCharVector[i] == 'T') &&
-				(( (giantCharVector[i + 1] == '-') &&
-				  (giantCharVector[i + 2] == 'T') &&
-				  (giantCharVector[i + 3] == 'h') )   ||
-				
-				 ( (giantCharVector[i + 1] == 'T') &&
-				  (giantCharVector[i + 2] == 'h') ))) {
-			listOfIndexesPotentialDays.push_back(i);
+		for (unsigned int j = 0; j < weekdays.size(); j++) {
+
+			string weekString = weekdays.at(j);
+
+			if (findStringReturnIndex(giantCharVector, weekString) > 0) {
+				listOfIndexesPotentialDays.push_back(findStringReturnIndex(giantCharVector, weekString));
+	
+			}
+
 		}
 	}
 
@@ -81,10 +98,7 @@ vector<unsigned int> potentialDayAtThisIndex(vector<char> giantCharVector) {
 
 
 
-
-
-// TODO: have this return a vector or string with zoom link
-// actually this now returns the index of the first dot
+// TODO: This ACTUALLY now returns the index of the first dot
 unsigned int numOfZoomLinks(vector<char> giantCharVector) {
 
 	unsigned int z = 0;
@@ -97,8 +111,8 @@ unsigned int numOfZoomLinks(vector<char> giantCharVector) {
 		    giantCharVector.at(i + 3) == 'o' &&
 		    giantCharVector.at(i + 4) == 'm' &&
 		    giantCharVector.at(i + 5) == '.' ) {
-//		       z++;	
-			z = i;
+
+		    z = i;
 		}
 	
 	}
@@ -106,74 +120,50 @@ unsigned int numOfZoomLinks(vector<char> giantCharVector) {
 	return z;
 }
 
-// TODO: have this return a vector or string with zoom link
-// the 'i' is the index of giantCharVector that the first dot is
-string returnZoomLink(vector<char> giantCharVector, unsigned int i) {
 
+string returnZoomLink(vector<char> giantCharVector, unsigned int i) {
 	//https://brandeis.zoom.us/j/91324843664
 	// to the left, look for a space, a '\', a ''', a '?', basically anything
 	// that isn't letters, the colon, dots, or forwardslashes.
 	// to the right, look for a space
 	string zoomLink; 
-	
 	char cursor;
 	int index;
 	string tempString;
-
 	index = i;
-//	cout << "index: " << index << endl;
 	cursor = giantCharVector.at(index);
 
-
-	while(cursor != ' ' && cursor != '=' && cursor != '(' && index > 0) {
-		// add the parts that go to the left
-
+	while(cursor != ' ' && cursor != '=' && cursor != '(' && cursor != ')' 
+			&& index > 0) {
 		index--;
-
 		cursor = giantCharVector.at(index);
-
-//		cout << "cursor: " << cursor << endl;
-
 		string tempString(1, cursor);		
-
 		tempString = tempString + zoomLink;
-//		cout << "tempString: " << tempString;
-
-		// need to make cursor a string TODO
 		zoomLink = tempString;
-
 	}
 
 	index = i;
 	cursor = giantCharVector.at(index);
-
 	//theoretically, i is:
 	//
 	// .  z  o  o  m  .
 	// ^ here
 	
-	while (cursor != ' ' && index  < giantCharVector.size() - 2) {
+	while (cursor != ' ' && cursor != '(' && cursor != ')' && 
+			index < giantCharVector.size() - 2) {
 		index++;
-
 		cursor = giantCharVector.at(index);
-
-//		tempString = to_string(cursor);
 		string tempString(1, cursor);
-
-		// need to make cursor a string TODO
-//		strcat(tempString, zoomLink);
 		tempString = zoomLink + tempString;
-
 		zoomLink = tempString;
-
 	}
 
-
-
-	return zoomLink;
+	if (i != 0) {
+		return zoomLink;
+	} else {
+		return "probably not a zoom link in this syllabus.";
+	}
 }
-
-
 
 
 int main() {
@@ -187,49 +177,79 @@ int main() {
 		fs >> temp;
 		allMonthStrings.push_back(temp);
 	}
-
-//	for (unsigned int i = 0; i < allMonthStrings.size(); ++i) {
-//		cout << allMonthStrings.at(i) << endl;
-//	}
 	fs.close();
 
-
-
-// notNames.txt
+	// notNames.txt
 	fs.open("notNames.txt");
 	vector<string> notNamesStrings;
 	while(!fs.eof()) {
 		fs >> temp;
 		notNamesStrings.push_back(temp);
 	}
-
-//	for (unsigned int i = 0; i < notNamesStrings.size(); ++i) {
-//		cout << notNamesStrings.at(i) << endl;
-//	}
 	fs.close();
 
-
-
-// titlesOfProf.txt
+	// titlesOfProf.txt
 	fs.open("titlesOfProf.txt");
 	vector<string> titlesOfProf;
 	while(!fs.eof()) {
 		fs >> temp;
 		titlesOfProf.push_back(temp);
 	}
-
-//	for (unsigned int i = 0; i < titlesOfProf.size(); ++i) {
-//		cout << titlesOfProf.at(i) << endl;
-//	}
 	fs.close();
 
+	// mon.txt
+	fs.open("mon.txt");
+	vector<string> mon;
+	while(!fs.eof()) {
+		fs >> temp;
+		mon.push_back(temp);
+	}
+	fs.close();
 
+	// tues.txt
+	fs.open("tues.txt");
+	vector<string> tues;
+	while(!fs.eof()) {
+		fs >> temp;
+		tues.push_back(temp);
+	}
+	fs.close();
 
-// testKeywords.txt
-// not doing this one, doesn't seem that needed.
+	// wed.txt
+	fs.open("wed.txt");
+	vector<string> wed;
+	while(!fs.eof()) {
+		fs >> temp;
+		wed.push_back(temp);
+	}
+	fs.close();
 
+	// thurs.txt
+	fs.open("thurs.txt");
+	vector<string> thurs;
+	while(!fs.eof()) {
+		fs >> temp;
+		thurs.push_back(temp);
+	}
+	fs.close();
 
+	// fri.txt
+	fs.open("fri.txt");
+	vector<string> fri;
+	while(!fs.eof()) {
+		fs >> temp;
+		fri.push_back(temp);
+	}
+	fs.close();
 
+	// weekdays.txt
+	fs.open("weekdays.txt");
+	vector<string> weekdays;
+	while(!fs.eof()) {
+		fs >> temp;
+		weekdays.push_back(temp);
+	}
+	fs.close();
 
 	// here we gooooo
 	fstream FS;
@@ -241,9 +261,7 @@ int main() {
 	FS.open(filename);
 
 	char tempChar;
-
 	string tempString;
-
 	vector<char> giantCharVector;
 
 	while(!FS.eof()) {
@@ -251,7 +269,7 @@ int main() {
 		getline(FS, tempString);
 		for (unsigned int i = 0; i < tempString.length(); ++i) {
 			tempChar = tempString.at(i);
-//			cout << tempChar;
+
 			giantCharVector.push_back(tempChar);
 		}
 
@@ -264,8 +282,6 @@ int main() {
 	unsigned int numCommas = 0;
 	unsigned int numMonths = 0;
 
-	//			size - 1, because the last one
-	//			is repeated because of .eof()
 	for (unsigned int i = 0; i < giantCharVector.size(); ++i) {
 		if (giantCharVector[i] == ' ') {
 			numWhitespace++;
@@ -297,66 +313,27 @@ int main() {
 	cout << "numColons: " << numColons << endl;
 	cout << "numCommas: " << numCommas << endl;
 	cout << "numNumbers: " << numNumbers << endl;
-
-//	cout << "numOfMonths: " << numOfMonths(allMonthStrings, giantCharVector);
-
 	cout << "index of a zoom link: " << numOfZoomLinks(giantCharVector) << endl;
-
-	cout << "zoom link:" << returnZoomLink(giantCharVector, 
+	cout << "zoom link: " << returnZoomLink(giantCharVector, 
 					numOfZoomLinks(giantCharVector));
 	cout << endl;
-
-
-
 	cout << endl;
-//	cout << "The most important thing I can do is get MWF xx:yy to aa::bb times";
-
 
 	vector<unsigned int> timeIndexes;
 	timeIndexes = potentialTimeAtThisIndex(giantCharVector);
 
-	cout << "special colons at: ";
-	for (unsigned int i = 0; i < timeIndexes.size(); i++) {
-//		cout << timeIndexes.at(i) << "   ";
-	}
-	cout << endl;
+	for (unsigned int i = 0; i < timeIndexes.size(); ++i) {
 
-	for (unsigned int j = 0; j < timeIndexes.size(); j++) {
-
-		for (unsigned int i = timeIndexes.at(j) - 2; i < timeIndexes.at(j) + 3; i++) {
-		
-		  
-			// really screws things up
-		    //  	if (giantCharVector.at(i) >= 48 && giantCharVector[i] <= 57) {
-		
-		    	cout << giantCharVector.at(i);
-		    
-		  //  }
-		
+		if (giantCharVector.at(timeIndexes.at(i) - 3) != '-') {
+			for (int j = -2; j < 3; ++j) {
+				cout << giantCharVector.at(  timeIndexes.at(i) + j  );
+				
+			} 
+			cout << "    ";
 		}
-		cout << "    ";
-
 	}
 
-	vector<unsigned int> dayIndexes;
-	dayIndexes = potentialDayAtThisIndex(giantCharVector);
 
-	for (unsigned int i = 0; i < dayIndexes.size(); i++) {
-		cout << dayIndexes.at(i) << "   ";
-	}
-	cout << endl;
-
-	for (unsigned int j = 0; j < dayIndexes.size(); j++) {
-
-		for (unsigned int i = dayIndexes.at(j) - 2; i < dayIndexes.at(j) + 3; i++) {
-			cout << giantCharVector.at(i);
-		}
-		cout << "    ";
-
-	}
-
-	cout << endl;
-	
 	return 0;
 }
 
